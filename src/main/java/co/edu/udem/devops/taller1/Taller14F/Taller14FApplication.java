27a06696ddbfeee07b6f65c602e33f69309175a4
Andres Rodriguez <andres@gmail.com>
2021-10-13 20:09:02 -0500

    Commit 1

---
+++ src/main/java/co/edu/udem/devops/taller1/Taller14F/Taller14FApplication.java
@@ -0,0 +1,13 @@
+package co.edu.udem.devops.taller1.Taller14F;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Taller14FApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Taller14FApplication.class, args);
+	}
+
+}
---
+++ src/test/java/co/edu/udem/devops/taller1/Taller14F/Taller14FApplicationTests.java
@@ -0,0 +1,13 @@
+package co.edu.udem.devops.taller1.Taller14F;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Taller14FApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
