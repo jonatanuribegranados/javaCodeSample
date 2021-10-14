7fe1337e54edf75db4a23d28b4b61267c701c3c1
Andres Rodriguez <andres@gmail.com>
2021-10-13 20:47:20 -0500

    Index

--- src/main/java/co/edu/udem/devops/taller1/Taller14F/Taller14FApplication.java
+++ src/main/java/co/edu/udem/devops/taller1/Taller14F/Taller14FApplication.java
@@ -7,6 +7,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 public class Taller14FApplication {
 
 	public static void main(String[] args) {
+		System.out.println("Hola mundo");
 		SpringApplication.run(Taller14FApplication.class, args);
 	}
 
